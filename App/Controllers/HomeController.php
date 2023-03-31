af8ac09f6fbc83b54f36acf614c090bc46e83142
Nguyễn Viết Huy <nguyenviethuy.jvb@gmail.com>
2023-03-31 08:26:52 +0700

    update fixbug HomeController

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -1,26 +1,16 @@
 <?php
 namespace App\Controllers;
 
-use App\Repositories\ExamRepository;
-use App\Services\ExportExamService;
-use ApplicationLoader;
-
 class HomeController extends AppController
 {
-    protected ExportExamService $service;
-    protected ExamRepository $repository;
 
     public function initialize()
     {
         parent::initialize();
-        $this->service = ApplicationLoader::service(ExportExamService::class);
-        $this->repository = ApplicationLoader::repository(ExamRepository::class);
     }
 
     public function index()
     {
-        $countExam = $this->repository->countExam();
-        dd($countExam);
         $this->render('index');
     }
 
